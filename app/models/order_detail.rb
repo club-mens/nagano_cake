cac45270429731d1dc30ec7050ffd350055d1bf3
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-09-22 07:15:05 +0000

    注文詳細画面記述完了動作確認未

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,12 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
+  validates :order_id, presence: true
+  validates :item_id, presence: true
+  validates :quantity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+  validates :price, presence: true
+  validates :making_status, presence: true
+  
   enum making_status: {
     着手不可: 0,
     制作待ち: 1,
