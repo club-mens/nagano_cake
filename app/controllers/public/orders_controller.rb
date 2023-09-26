4cbcb1fd2afb7aae3633705579a2a3bb07ec3a7d
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-09-26 10:30:37 +0000

    エラー訂正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,37 +1,34 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
 
   def new
     @order = Order.new
-    @addresses = current_customer.addresses
   end
 
-  def confirm
-    @cart_items = current_customer.cart_items.all
+ def confirm
     @order = Order.new(order_params)
-    if params[:order][:address_select] == 'own_address'
-      @order.post_code = current_customer.post_code
-      @order.address = current_customer.address
+    if params[:order][:select_address] == "0"
       @order.name = current_customer.full_name
-    elsif params[:order][:address_select] == 'registered_address'
+      @order.address = current_customer.address
+      @order.post_code = current_customer.post_code
+    elsif params[:order][:select_address] == "1"
       @address = Address.find(params[:order][:address_id])
       @order.post_code = @address.post_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:order][:address_select] == 'new_address'
-      @order = Order.new(order_params)
-      address = Address.new(new_address_params)
-      session[:address] = address
+    elsif params[:order][:select_address] == "2"
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+      unless @order.valid_columns?(:name, :address, :post_code)
+        @addresses = current_customer.addresses
+        render :new
+      end
+    else
+      redirect_to new_order_path
     end
-    @cart_items = current_customer.cart_items.all
-    total_price = 0
-    @cart_items.each do |cart_item|
-      total_price = total_price + cart_item.item.price
-    end
-    @order.total_price = (total_price*1.1).floor
-    session[:order] = @order
-    redirect_to check_orders_path
+    @cart_items = current_customer.cart_items
   end
 
   def complete
@@ -39,26 +36,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    cart_items = current_customer.cart_items.all
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
     @order.save
-    cart_items.each do |cart_item|
-      order_item = OrderItem.new
-      order_item.order_id = @order.id
-      order_item.item_id = cart_item.item.id
-      order_item.quantity = cart_item.quantity
-      order_item.price = cart_item.item.price*1.1
-      order_item.save
+
+    current_customer.cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.amount = cart_item.amount
+      @order_detail.price_including_tax = (cart_item.item.price_without_tax * 1.1).floor
+      @order_detail.order_id =  @order.id
+      @order_detail.save
     end
-    if session[:address]
-      address = Address.new(session[:address])
-      address.save
-    end
-    session.delete(:order)
-    session.delete(:address)
-    cart_items.destroy_all
+
+    current_customer.cart_items.destroy_all
     redirect_to complete_orders_path
   end
+
 
 
   def index
