6a421f8a3fead8e0f4f7ac983c2030694590a5b1
kitamura-ryusei <141385505+kitamura-ryusei@users.noreply.github.com>
2023-09-26 03:33:01 +0000

    顧客マイページ完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,25 +1,29 @@
 class Public::CustomersController < ApplicationController
-  before_action :authenticate_public!
+  before_action :authenticate_customer!
 
 
   def show
- 
+    @customer = current_customer
   end
 
   def edit
-
+    @customer = current_customer
   end
 
   def update
-  
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :edit
+    end
   end
 
   def confirm
-
   end
 
   def withdrawal
-
+    
   end
 
 end
