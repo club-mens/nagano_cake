a82c50ca2b6fbce66a84bb116b2aa1185486e8b8
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-09-27 11:36:50 +0000

    エラー解消

--- db/migrate/20230920123830_create_orders.rb
+++ db/migrate/20230920123830_create_orders.rb
@@ -6,13 +6,13 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.integer :postage, null: false
 
       #default値がわからん
-      t.string :status, null: false, default: 0
+      t.integer :status, null: false, default: 0
 
       t.string :post_code, null: false
       t.string :address, null: false
       t.string :name, null: false
       
-      t.string :payment_method, null: false
+      t.integer :payment_method, null: false
 
       t.timestamps
     end
